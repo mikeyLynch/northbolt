6bfab09e6c0f72c5acf053b584a762db19f6b2d6
Michael <mjlynch202@gmail.com>
2026-05-13 16:04:58 +0100

    connection

--- app/models/lock.rb
+++ app/models/lock.rb
@@ -1,6 +1,16 @@
 class Lock < ApplicationRecord
   belongs_to :location
 
+  CONNECTIVITY_THRESHOLD = 10.minutes
+
   validates :unit_identifier, presence: true
   validates :device_uuid, presence: true, uniqueness: true, format: { with: /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i }
+
+  def probably_online?
+    last_seen_at.present? && last_seen_at >= CONNECTIVITY_THRESHOLD.ago
+  end
+
+  def probably_offline?
+    !probably_online?
+  end
 end
--- spec/models/lock_spec.rb
+++ spec/models/lock_spec.rb
@@ -8,4 +8,30 @@ RSpec.describe Lock, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:unit_identifier) }
   end
+
+  describe "#probably_online?" do
+    it "returns true when last_seen_at is within 10 minutes" do
+      lock = build(:lock, last_seen_at: 5.minutes.ago)
+      expect(lock.probably_online?).to be true
+    end
+
+    it "returns false when last_seen_at is more than 10 minutes ago" do
+      lock = build(:lock, last_seen_at: 11.minutes.ago)
+      expect(lock.probably_online?).to be false
+    end
+
+    it "returns false when last_seen_at is nil" do
+      lock = build(:lock, last_seen_at: nil)
+      expect(lock.probably_online?).to be false
+    end
+  end
+
+  describe "#probably_offline?" do
+    it "is the inverse of probably_online?" do
+      online = build(:lock, last_seen_at: 1.minute.ago)
+      offline = build(:lock, last_seen_at: 11.minutes.ago)
+      expect(online.probably_offline?).to be false
+      expect(offline.probably_offline?).to be true
+    end
+  end
 end
